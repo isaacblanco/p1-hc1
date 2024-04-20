53c0823261ce158dbcea9d3023e735e9634e0945
Isaac Blanco <63171746+isaacblanco@users.noreply.github.com>
2024-04-20 16:20:54 +0200

    Unstable changes
    Just to save this state

---
+++ src/assets/data.js
@@ -0,0 +1,18 @@
+const data = {
+  albums: [
+    {
+      title: "",
+      year: "",
+      artist: "Disturbed",
+      urlCover: "",
+      songs: [
+        {
+          pos: 1,
+          title: "",
+          m: 3,
+          s: 45,
+        },
+      ],
+    },
+  ],
+};
